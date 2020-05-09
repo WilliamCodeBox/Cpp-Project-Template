4e54a81a40f3e3c23890521cf87cd54b62b4f474
Liu Chang <luroylau@163.com>
2020-05-09 13:42:56 +0800

    use exhale for automating

---
+++ docs/conf.py
@@ -0,0 +1,121 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# import sys
+# sys.path.insert(0, os.path.abspath('.'))
+
+# -- Project information -----------------------------------------------------
+
+project = 'cpp=project-template'
+copyright = '2020, WilliamCodeBox'
+author = 'WilliamCodeBox'
+
+# The full version, including alpha/beta/rc tags
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+# The `extensions` list should already be in here from `sphinx-quickstart`
+extensions = [
+    # there may be others here already, e.g. 'sphinx.ext.mathjax'
+    'breathe',
+    'exhale'
+]
+
+# Setup the breathe extension
+breathe_projects = {"My Project": "./doxyoutput/xml"}
+breathe_default_project = "My Project"
+
+
+# somewhere in `conf.py`, *BERORE* declaring `exhale_args`
+def specificationsForKind(kind):
+    '''
+    For a given input ``kind``, return the list of reStructuredText specifications
+    for the associated Breathe directive.
+    '''
+    # Change the defaults for .. doxygenclass:: and .. doxygenstruct::
+    if kind == "class" or kind == "struct":
+        return [
+            ":members:", ":protected-members:", ":private-members:",
+            ":undoc-members:"
+        ]
+    # Change the defaults for .. doxygenenum::
+    elif kind == "enum":
+        return [":no-link:"]
+    # An empty list signals to Exhale to use the defaults
+    else:
+        return []
+
+
+# Use exhale's utility function to transform `specificationsForKind`
+# defined above into something Exhale can use
+from exhale import utils
+# Setup the exhale extension
+exhale_args = {
+    # These arguments are required
+    "containmentFolder":
+    "./api",
+    "rootFileName":
+    "library_root.rst",
+    "rootFileTitle":
+    "Library API",
+    "doxygenStripFromPath":
+    "..",
+    # Suggested optional arguments
+    "createTreeView":
+    True,
+    # TIP: if using the sphinx-bootstrap-theme, you need
+    # "treeViewIsBootstrap": True,
+    "exhaleExecutesDoxygen":
+    True,
+    "exhaleDoxygenStdin":
+    "INPUT = ../include",
+    "customSpecificationsMapping":
+    utils.makeCustomSpecificationsMapping(specificationsForKind)
+}
+
+# Tell sphinx what the primary language being documented is.
+primary_domain = 'cpp'
+
+# Tell sphinx what the pygments highlight language should be.
+highlight_language = 'cpp'
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
